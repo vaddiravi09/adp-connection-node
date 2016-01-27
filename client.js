6c61f7f27f5d9c92b432dbcb7f1e566647600add
steversj <Joshua.Steverson-Contractor@adp.com>
2016-01-26 22:04:18 -0500

    removing event emitter since it caused issues with throwing exceptions when desired. Adding a few exceptions to be raised upon failed validation of config.

--- client.js
+++ client.js
@@ -12,19 +12,14 @@ var options = {
 	associateoid: 'G3BYYP2D9XJ4CVTF',
 	orgoid: 'G3BYYP2D9XJ4Z8WK'
 };
-adp.connect(options);
-adp.on('error', (err) => {
-	log.error('Error received from ADP client.' + err);
-});
-adp.on('connected', (data) => {
-	log.info(JSON.stringify(data));
+adp.connect(options, () => {
 	worker = adp.apiProduct('Worker');
 	if(worker) {
 		worker.call('read', (err2, data2) => {
 			if(err2) {
 				log.error('Error returned from worker call.');
 			}
-			log.info('RETURN WORKER READ - ' + + JSON.stringify(data2));
+			log.info('RETURN WORKER READ - ' + JSON.stringify(data2));
 		});
 	}
 	userInfo = adp.apiProduct('UserInfo');
