7f641cce7e354116cb4c19429e4ea1c93fc69bc1
steversj <Joshua.Steverson-Contractor@adp.com>
2016-02-11 18:01:37 -0500

    changing lib entry point code

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict';
+/*'use strict';
 
 var server = require('./server');
 var log = require('winston');
@@ -6,3 +6,18 @@ var log = require('winston');
 server.start(function startCb() {
 	log.info('Server started callback');
 });
+*/
+
+'use strict';
+
+var ADPAPIConnectionFactory = require('./lib/adpApiConnectionFactory');
+var ADPAPIProductFactory = require('./lib/adpApiProductFactory');
+var ClientCredentialsConnType = require('./lib/clientCredentialsConnType');
+var AuthorizationCodeConnType = require('./lib/authorizationCodeConnType');
+
+module.exports = {
+	ADPAPIConnectionFactory: ADPAPIConnectionFactory,
+	AuthorizationCodeConnType: AuthorizationCodeConnType,
+	ClientCredentialsConnType: ClientCredentialsConnType,
+	ADPAPIProductFactory: ADPAPIProductFactory
+};
