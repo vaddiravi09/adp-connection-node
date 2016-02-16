cbf5684ecf392608a3893adfda49ae10f64733b7
steversj <Joshua.Steverson-Contractor@adp.com>
2016-02-16 11:06:57 -0500

    removing mongo dependency

--- server.js
+++ server.js
@@ -2,32 +2,22 @@
 
 var config = require('config');
 var server;
-var mongoose = require('mongoose');
 var express = require('express');
-var DB = config.get('db.connection');
 var PORT = config.get('server.port');
 var routes = require('./lib/routes');
 var log = require('winston');
 
 function start(cb) {
-	mongoose.connect(DB);
-	var db = mongoose.connection;
-	db.on('error', cb);
-	db.once('open', function openCb() {
-		log.info('Database connection complete.');
-		var app = express();
-		app.use(routes);
-		app.use(express.static('web'));
-		server = app.listen(PORT, cb);
-	});
+	var app = express();
+	app.use(routes);
+	app.use(express.static('web'));
+	server = app.listen(PORT, cb);
 }
 
 function stop(cb){
 	try{
 		server.close(function serverStopCb() {
-			mongoose.disconnect(function mongooseStopCb(){
-				if(cb) { cb(); }
-			});
+			if(cb) { cb(); }
 		});
 	}catch(e){
 		if(cb) { return cb(e); }
