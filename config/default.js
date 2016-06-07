77daeee6a09c7bd38be7b643c7143ca5ec696869
JoshSteverson <Joshua.Steverson-Contractor@adp.com>
2016-06-07 16:58:30 -0400

    clean up commented code

--- config/default.js
+++ config/default.js
@@ -1,25 +1,11 @@
 'use strict';
-/*
-var testconnection = {
-	apiUrl: 'http://localhost:55555/api',
-	tokenUrl: 'http://localhost:55555/token',
-	authorizationUrl: 'http://localhost:55555/authorize'
-};
-*/
+
 var connection = {
 	apiUrl: 'https://iat-api.adp.com',
 	tokenUrl: 'https://iat-api.adp.com/auth/oauth/v2/token',
 	authorizationUrl: 'https://iat-accounts.adp.com/auth/oauth/v2/authorize'
 };
-/*
-var ditconnection = {
-	apiUrl: 'https://accountsdit.nj.adp.com',
-	tokenUrl: 'https://accountsdit.nj.adp.com/auth/oauth/v2/token',
-	authorizationUrl: 'https://accountsdit.nj.adp.com/auth/oauth/v2/authorize'
-};
-var conn = process.env.NODE_ENV === 'TEST' ? testconnection : connection;
-conn = process.env.NODE_ENV === 'DIT' ? ditconnection : conn;
-*/
+
 module.exports = {
 	db: {
 		connection: 'localhost:27017'
