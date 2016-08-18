24187e8c62498ca6ddac094afa8b794718088543
Josh Steverson <josh.steverson@gmail.com>
2016-08-18 07:34:44 -0400

    updating to v2 example scripts

--- clientCredentialsExample.js
+++ clientCredentialsExample.js
@@ -2,33 +2,24 @@
 
 process.env.NODE_DEBUG = 'adp-connection';
 
-var adp = require('./index');
-var ClientCredentialsConnType = adp.ClientCredentialsConnType;
-var ConnectionFactory = adp.ADPAPIConnectionFactory;
 var debug = require('./lib/debug');
+var adpConnection = require('./index');
 
-var connType = new ClientCredentialsConnType();
-var connectionFactory = new ConnectionFactory();
-
-var initObject = {
+var connectionOpts = {
 	clientId: '88a73992-07f2-4714-ab4b-de782acd9c4d',
 	clientSecret: 'a130adb7-aa51-49ac-9d02-0d4036b63541',
-	apiUrl: 'https://iat-api.adp.com',
-	tokenUrl: 'https://iat-api.adp.com/auth/oauth/v2/token'
+	granttype: 'client_credentials',
+	sslCertPath: 'iatCerts/apiclient_iat.pem',
+	sslKeyPath: 'iatCerts/apiclient_iat.key'
 };
-connType.init(initObject);
-connType.setSSLCertPath('iatCerts/apiclient_iat.pem');
-connType.setSSLKeyPath('iatCerts/apiclient_iat.key');
 
-var options = {
-	keepAlive: true
-};
-var connection = connectionFactory.createConnection('client_credentials');
-connection.init(connType);
-connection.connect(options, function connectCb(err){
+var conn = adpConnection.createConnection(connectionOpts);
+function connectionComplete(err) {
 	if(err) {
 		debug('Not Connected');
 	}else{
-		debug('Connected with token: ' + connection.accessToken);
+		debug('Connected with token: ' + conn.accessToken);
 	}
-});
+}
+conn.connect(connectionComplete);
+
--- routes.js
+++ routes.js
@@ -18,35 +18,27 @@ governing permissions and limitations under the License.
 
 var express = require('express');
 var router = express.Router();
-var debug = require('./debug');
-var adp = require('../index');
-var ConnectionFactory = adp.ADPAPIConnectionFactory;
-var AuthorizationCodeConnType = adp.AuthorizationCodeConnType;
+var debug = require('./lib/debug');
+var adpConnection = require('./index');
 
 router.get('/callback', function callback(req, res){
 
-	var connectionFactory = new ConnectionFactory();
-	var connType = new AuthorizationCodeConnType();
-	var initObject = {
-		clientId: 'ec762f06-7410-4f6d-aa82-969902c1836a',
-		clientSecret: '6daf2cd7-4604-46c0-ab43-a645a6571d34',
-		apiUrl: 'https://iat-api.adp.com',
-		tokenUrl: 'https://iat-api.adp.com/auth/oauth/v2/token',
-		authorizationUrl: 'https://iat-accounts.adp.com/auth/oauth/v2/authorize',
+	var connectionOpts = {
+		clientId: '9da768af-1ef3-4783-b599-d637875a68e2',
+		clientSecret: '524c0109-cdc4-46c0-973b-da2f7e6df88b',
 		sslKeyPath: 'iatCerts/apiclient_iat.key',
 		sslCertPath: 'iatCerts/apiclient_iat.pem',
 		callbackUrl: 'http://localhost:8889/callback',
+		granttype: 'authorization_code',
 		authorizationCode: req.query.code
 	};
-	connType.init(initObject);
 
-	var connection = connectionFactory.createConnection('authorization_code');
-	connection.init(connType);
-	connection.connect(null, function connectCb(err) {
+	var conn = adpConnection.createConnection(connectionOpts);
+	conn.connect(function connectCb(err) {
 		if(err) {
 			res.send('Not Connected');
 		}else{
-			res.send('Connected with token: ' + connection.accessToken);
+			res.send('Connected with token: ' + conn.accessToken);
 		}
 		res.end();
 	});
@@ -54,22 +46,16 @@ router.get('/callback', function callback(req, res){
 });
 
 router.get('/authenticate', function login(req, res) {
-	var connType = new AuthorizationCodeConnType();
-	var initObject = {
-		clientId: 'ec762f06-7410-4f6d-aa82-969902c1836a',
-		clientSecret: '6daf2cd7-4604-46c0-ab43-a645a6571d34',
-		apiUrl: 'https://iat-api.adp.com',
-		tokenUrl: 'https://iat-api.adp.com/auth/oauth/v2/token',
-		authorizationUrl: 'https://iat-accounts.adp.com/auth/oauth/v2/authorize',
+	var connectionOpts = {
+		clientId: '9da768af-1ef3-4783-b599-d637875a68e2',
+		clientSecret: '524c0109-cdc4-46c0-973b-da2f7e6df88b',
 		sslKeyPath: 'iatCerts/iat.key',
 		sslCertPath: 'iatCerts/iat.pem',
+		granttype: 'authorization_code',
 		callbackUrl: 'http://localhost:8889/callback'
 	};
-	connType.init(initObject);
-	var connectionFactory = new ConnectionFactory();
-	var connection = connectionFactory.createConnection('authorization_code');
-	connection.init(connType);
-	var url = connection.getAuthorizationRequest();
+	var conn = adpConnection.createConnection(connectionOpts);
+	var url = conn.getAuthorizationRequest();
 	debug('Redirecting: ' + url);
 	res.redirect(url);
 });
